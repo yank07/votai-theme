613976f0afdab0c76852add943175f46c8b543ae
Felipe Álvarez <luisfelipealvarez@gmail.com>
2015-06-22 00:22:08 -0300

    Si apuntas a /theme/election/<election-slug>/media-naranja.json obtendras un json como el descrito en https://gist.github.com/martinszy/c66792d632749661198c

--- votai_theme/urls.py
+++ votai_theme/urls.py
@@ -1,9 +1,11 @@
-from django.conf import settings
 from django.conf.urls import patterns, url
-from votai_theme.views import MediaNaranjaView
+from votai_theme.views import MediaNaranjaView, MedianaranjaJsonView
 
 urlpatterns = patterns('',
-                      url(r'^election/(?P<slug>[-\w]+)/media-naranja/?$',
-                          MediaNaranjaView.as_view(template_name='elections/soulmate_candidate.html'),
-                          name='soul_mate_detail_view'),
-                      )
+                       url(r'^election/(?P<slug>[-\w]+)/media-naranja/?$',
+                           MediaNaranjaView.as_view(template_name='elections/soulmate_candidate.html'),
+                           name='soul_mate_detail_view'),
+                       url(r'^election/(?P<slug>[-\w]+)/media-naranja\.json$',
+                           MedianaranjaJsonView.as_view(),
+                           name='soul_mate_detail_view_json'),
+                       )
