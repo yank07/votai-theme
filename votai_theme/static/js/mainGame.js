27a9f0d7751128b46dc92a975f0c89cab56ed519
jpamato <juanpamato@gmail.com>
2015-07-20 23:51:23 -0300

    estilos arreglados y fotos en orden

--- votai_theme/static/js/mainGame.js
+++ votai_theme/static/js/mainGame.js
@@ -148,7 +148,7 @@ var app = (function(){
 				}else{
 					punParcial[i][0] = -1;
 				}
-				//console.log("PunPar "+candidatos[cInd]["candidate_name"]+": "+punParcial[i]);
+				console.log("PunPar "+candidatos[cInd]["candidate_name"]+": "+punParcial[i]);
 			}
 			
 		}	
