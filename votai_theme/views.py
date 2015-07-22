9994c86241c5ccf8f15fe4c7a59b922378b0f575
Felipe Álvarez <falvarez@votainteligente.cl>
2015-07-22 15:14:52 -0300

    Ahora cada uno de los extrainfo aparecerán en el json

--- votai_theme/views.py
+++ votai_theme/views.py
@@ -7,7 +7,6 @@ from sorl.thumbnail import get_thumbnail
 from django.views.decorators.clickjacking import xframe_options_exempt
 
 
-
 class juegoView(SoulMateDetailView):
     @xframe_options_exempt
     def dispatch(self, *args, **kwargs):
@@ -16,10 +15,11 @@ class juegoView(SoulMateDetailView):
 
 class MediaNaranjaView(SoulMateDetailView):
     calculator_class = YQSCalculator
+
     @xframe_options_exempt
     def dispatch(self, *args, **kwargs):
         return super(MediaNaranjaView, self).dispatch(*args, **kwargs)
-	
+
 
 class MedianaranjaJsonView(MediaNaranjaView):
     @xframe_options_exempt
@@ -59,21 +59,23 @@ class MedianaranjaJsonView(MediaNaranjaView):
             response['categories'].append(category_dict)
         for candidate in election.candidates.all():
 
-            imurl =  candidate.extra_info['portrait_photo'];
+            imurl = candidate.extra_info['portrait_photo']
             try:
-              im = get_thumbnail(candidate.image, '100x100', format='PNG', crop='center', quality=99);
-              if im is not None: imurl = im.url;
-            
+                im = get_thumbnail(candidate.image, '100x100', format='PNG', crop='center', quality=99)
+                if im is not None:
+                    imurl = im.url
+
             except e:
-		pass
+                pass
 
             candidate_dict = {'candidate_id': candidate.id,
                               'candidate_name': candidate.name,
                               'candidate_pic': imurl,
-                              'candidate_color': candidate.extra_info['color'],
                               'positions': []
                               }
-            
+            for key in candidate.extra_info.keys():
+                candidate_dict['candidate_' + key] = candidate.extra_info[key]
+
             taken_positions = TakenPosition.objects.filter(position__topic__category__in=election.categories.all(), person=candidate)
             for taken_position in taken_positions:
                 taken_position_dict = {'question_id': taken_position.topic.id,
