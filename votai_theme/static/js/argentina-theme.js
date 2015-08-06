430fb839af4c88ec214ee66ea0f703b8dcc9469d
@martinszy <martinsz@gmail.com>
2015-08-06 18:10:44 -0300

    Agregando flecha para volver arriba en páginas largas

--- votai_theme/static/js/argentina-theme.js
+++ votai_theme/static/js/argentina-theme.js
@@ -3,7 +3,7 @@ function getDayCount(){
 		var firstDate = new Date();
 		var secondDate = new Date(2015,07,09);
 
-		var diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
+		var diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)))+1;
 		return diffDays;
 	}
 
@@ -42,3 +42,34 @@ if (window.elections_json) {
         });        
 }
 
+
+
+
+jQuery(document).ready(function($){
+        // browser window scroll (in pixels) after which the "back to top" link is shown
+        var offset = $(window).innerHeight() + 100,
+                //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+                offset_opacity = 1200,
+                //duration of the top scrolling animation (in ms)
+                scroll_top_duration = 700,
+                //grab the "back to top" link
+                $back_to_top = $('.cd-top');
+
+        //hide or show the "back to top" link
+        $(window).scroll(function(){
+                ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+                if( $(this).scrollTop() > offset_opacity ) { 
+                        $back_to_top.addClass('cd-fade-out');
+                }
+        });
+
+        //smooth scroll to top
+        $back_to_top.on('click', function(event){
+                event.preventDefault();
+                $('body,html').animate({
+                        scrollTop: 0 ,
+                        }, scroll_top_duration
+                );
+        });
+
+});
