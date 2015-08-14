5ea94cbe332fa748dce6cead57df092631e2e26c
@martinszy <martinsz@gmail.com>
2015-08-14 01:50:53 -0300

    Acualizando calendario electoral. Relacionado con #56

--- votai_theme/static/js/argentina-theme.js
+++ votai_theme/static/js/argentina-theme.js
@@ -1,49 +1,5 @@
-function getDayCount(){
-		var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
-		var firstDate = new Date();
-		var secondDate = new Date(2015,07,09);
-
-		var diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)))+1;
-		return diffDays;
-	}
-
-$(".faltan").html("Calendario electoral: faltan <span class='dias'>"+getDayCount()+" d&#237;as</span> para las PASO");
 var elecUrl="/election/pre-candidato-a-presidente";
 var jsonUrl= "/theme/election/pre-candidato-a-presidente/media-naranja.json";
-if (window.elections_json) {        
-	jsonUrl= elections_json[0].medianaranja_link;
-
-        var options_eleccion = '';
-        options_eleccion += '<option value="' + elecUrl + '"><h4>Elegí tu distrito</h4><\/option>';
-        $.each(elections_json, function(key,value){
-                //console.log(value["detaillink"]);
-                options_eleccion += '<option value="' + value["detaillink"] + '"><h4>' +  value["name"] + '</h4><\/option>';
-        });
-        $("select#eleccion").html(options_eleccion);
-
-        $( "select#eleccion,#menuMob select" ).change( function(e){
-                elecUrl=$(e.target).val();
-		if ($(".game-cta").length > 0) {
-			location.href=elecUrl;
-		}
-        });
-
-        function jugar(){
-
-        //url = "/theme/election/pre-candidato-a-presidente/media-naranja.json";
-                location.href="/theme"+elecUrl+"/media-naranja";
-                jsonUrl=elecUrl+"/media-naranja.json";
-
-        }
-
-
-        $(".bEmpez").click(function() {
-                jugar();
-        });        
-}
-
-
-
 
 jQuery(document).ready(function($){
         // browser window scroll (in pixels) after which the "back to top" link is shown
@@ -72,4 +28,53 @@ jQuery(document).ready(function($){
                 );
         });
 
+        //Calendario electoral
+        $(".fecha-distancia").each(function(i,el) {
+                dateParts = $(el).text().split('/');
+                date = new Date(dateParts[1] + "-" + dateParts[0] + "-" + dateParts[2]);
+                $(el).text(getDayCount(date) + " días");
+        });
+
+
+        //Init election selector
+        if (window.elections_json) {        
+                jsonUrl= elections_json[0].medianaranja_link;
+
+                var options_eleccion = '';
+                options_eleccion += '<option value="' + elecUrl + '"><h4>Elegí tu distrito</h4><\/option>';
+                $.each(elections_json, function(key,value){
+                        //console.log(value["detaillink"]);
+                        options_eleccion += '<option value="' + value["detaillink"] + '"><h4>' +  value["name"] + '</h4><\/option>';
+                });
+                $("select#eleccion").html(options_eleccion);
+
+                $( "select#eleccion,#menuMob select" ).change( function(e){
+                        elecUrl=$(e.target).val();
+                        if ($(".game-cta").length > 0) {
+                                location.href=elecUrl;
+                        }
+                });
+
+                function jugar(){
+
+                //url = "/theme/election/pre-candidato-a-presidente/media-naranja.json";
+                        location.href="/theme"+elecUrl+"/media-naranja";
+                        jsonUrl=elecUrl+"/media-naranja.json";
+
+                }
+
+
+                $(".bEmpez").click(function() {
+                        jugar();
+                });        
+        }
 });
+
+
+function getDayCount(secondDate){
+        var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+        var firstDate = new Date();
+        console.log(firstDate,secondDate);
+        var diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)))+1;
+        return diffDays;
+}
