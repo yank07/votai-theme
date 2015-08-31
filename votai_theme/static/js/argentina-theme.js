a8072998d9e434e157c0315c8360c39ab7bd112d
@martinszy <martinsz@gmail.com>
2015-08-31 14:17:53 -0300

    Volviendo a poner elecciones generales en la home.

--- votai_theme/static/js/argentina-theme.js
+++ votai_theme/static/js/argentina-theme.js
@@ -1,5 +1,5 @@
-var elecUrl="/election/pre-candidato-a-gobernador-de-tucuman";
-var jsonUrl= "/theme/election/pre-candidato-a-gobernador-de-tucuman/media-naranja.json";
+var elecUrl="/election/pre-candidato-a-presidente";
+var jsonUrl= "/theme/election/pre-candidato-a-presidente/media-naranja.json";
 
 jQuery(document).ready(function($){
         // browser window scroll (in pixels) after which the "back to top" link is shown
