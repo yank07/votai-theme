bd351e86deb9b56a545c0f5175a937c13d1389b5
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2015-10-16 17:20:58 -0300

    Autocreando taken positions por candidato

--- votai_theme/admin.py
+++ votai_theme/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from votai_theme.models import AnswerValue
+from elections.models import Topic
 from elections.admin import TakenPositionInline
-from elections.admin import Position
-	
+from elections.admin import Position, TopicModelForm
+from django import forms
+from django.utils.encoding import python_2_unicode_compatible
+from candidator.models import TakenPosition
+
+    
 class AnswerValueInline(admin.TabularInline):
     model = AnswerValue
     extra = 1
@@ -14,3 +19,33 @@ class YQSPositionAdmin(admin.ModelAdmin):
 
 admin.site.unregister(Position)
 admin.site.register(Position, YQSPositionAdmin)
+
+
+class YQSPosition(Position):
+    def save(self, *args, **kwargs):
+        super(YQSPosition, self).save(*args, **kwargs)
+        for candidate in self.topic.election.candidates.all():
+            TakenPosition.objects.get_or_create(topic=self.topic, position=self, person=candidate)
+
+
+    class Meta:
+        proxy = True
+
+
+class YQSPositionForm(forms.ModelForm):
+    model = YQSPosition
+
+
+class YQSPositionInline(admin.TabularInline):
+    model = YQSPosition
+    form = YQSPositionForm
+
+
+class TopicAdmin(admin.ModelAdmin):
+    inlines = [YQSPositionInline, ]
+    form = TopicModelForm
+    list_display = ('__str__', 'election')
+    search_fields = ['label', 'category__name']
+
+admin.site.unregister(Topic)
+admin.site.register(Topic, TopicAdmin)
