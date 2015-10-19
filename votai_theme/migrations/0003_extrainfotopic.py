a0c4d93aaa40261952d9cf05692a6b9684d20dfb
Martín Szyszlican <martinsz@gmail.com>
2015-10-19 17:21:18 -0300

    Eliminando dependencias de las migraciones para que no se rompa el build

--- votai_theme/migrations/0003_extrainfotopic.py
+++ votai_theme/migrations/0003_extrainfotopic.py
@@ -7,7 +7,6 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('elections', '0021_candidate_did_not_pass_primaries'),
         ('votai_theme', '0002_topic'),
     ]
 
